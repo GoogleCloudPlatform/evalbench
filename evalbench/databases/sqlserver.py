38862de3cf44c1e5baff8d3c2047e1c4d87759df
Jahnavi Malhotra <jahnaviii@google.com>
2025-02-25 04:56:13 +0000

    Fixing linting

--- evalbench/databases/sqlserver.py
+++ evalbench/databases/sqlserver.py
@@ -35,6 +35,7 @@ GRANT SELECT, INSERT, UPDATE, DELETE ON `{DATABASE}`.* TO "{DML_USERNAME}"@"%";
 FLUSH PRIVILEGES;
 """
 
+
 class SQLServerDB(DB):
 
     def __init__(self, db_config):
@@ -249,7 +250,7 @@ class SQLServerDB(DB):
                 DROP_TABLE_COMMAND.format(TABLE_NAME=table["name"])
             )
         self.batch_execute(drop_all_tables_commands)
-        
+ 
     def insert_data(self, data: dict[str, List[str]]):
         if not data:
             return
